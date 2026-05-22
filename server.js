0faca33373ff2b568faff757146282b076160cb8
vijaybhadade <vijaybhadade01@gmail.com>
2026-05-22 13:47:22 +0530

    fix frontend static deployment

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const app = express();
+
 const userNotes = require("./routers/notes");
 const userRouter = require("./routers/userRouter");
-const cors=require("cors");
+
+const cors = require("cors");
 
 app.use(cors());
-app.use(express.static("public"));
 app.use(express.json());
+app.use(express.static("public"));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/notes", userNotes);
 app.use("/users", userRouter);
 
-
-// Removed (req, res) from the callback
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
