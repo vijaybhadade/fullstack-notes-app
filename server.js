16187edd01fcc880d55741b4fba9b5b98e88151e
vijaybhadade <vijaybhadade01@gmail.com>
2026-05-22 13:56:13 +0530

    fix root route login page

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const app = express();
 
 const userNotes = require("./routers/notes");
 const userRouter = require("./routers/userRouter");
-
+const path = require("path");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "login.html"));
+});
 
 const port = process.env.PORT || 3000;
 
